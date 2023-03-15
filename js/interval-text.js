b4e5c5bf1242cebcb953ca1b4d74c34be3d8e610
iconcert8 <51566869+iconcert8@users.noreply.github.com>
2023-03-15 12:42:56 +0900

    FEATURE: add spring-aop

--- js/interval-text.js
+++ js/interval-text.js
@@ -1,28 +1,28 @@
 function registerMouseHoverEvent(element, initialHTML, prefix, sufix, text){
-    let intervalId = 0;
-    element.onmouseenter = ()=>{
-      if(intervalId != 0){
-        return;
-      }
-      intervalId = writeIntervalText(element, prefix, sufix, text);
+  let intervalId = 0;
+  element.onmouseenter = ()=>{
+    if(intervalId != 0){
+      return;
     }
-
-    element.onmouseleave = ()=>{
-      if(intervalId != 0){
-        clearInterval(intervalId);
-        intervalId = 0;
-      }
-      element.innerHTML = initialHTML;
-    }
+    intervalId = writeIntervalText(element, prefix, sufix, text);
   }
 
-  function writeIntervalText(element, prefix, sufix, text){
-    const words = text.split('');
-    let i = 0;
-    let str = "";
-    return setInterval(()=>{
-      if(i >= words.length) return;
-      str += words[i++];
-      element.innerHTML = prefix + str + sufix;
-    }, 170);
+  element.onmouseleave = ()=>{
+    if(intervalId != 0){
+      clearInterval(intervalId);
+      intervalId = 0;
+    }
+    element.innerHTML = initialHTML;
   }
+}
+
+function writeIntervalText(element, prefix, sufix, text){
+  const words = text.split('');
+  let i = 0;
+  let str = "";
+  return setInterval(()=>{
+    if(i >= words.length) return;
+    str += words[i++];
+    element.innerHTML = prefix + str + sufix;
+  }, 170);
+}
