f4f0fcdcf1b36c3c280d8c5fdc6a47d1cf1f9c0a
iconcert8 <51566869+iconcert8@users.noreply.github.com>
2023-04-12 20:50:52 +0900

    FIX: interval clear

--- js/interval-text.js
+++ js/interval-text.js
@@ -1,20 +1,20 @@
-function registerMouseHoverWriteIntevalText(element, initialHTML, prefix, sufix, text){
-  let intervalId = 0;
-  element.onmouseenter = ()=>{
-    if(intervalId != 0){
-      return;
-    }
-    intervalId = writeIntervalText(element, prefix, sufix, text);
-  }
+// function registerMouseHoverWriteIntevalText(element, initialHTML, prefix, sufix, text){
+//   let intervalId = 0;
+//   element.onmouseenter = ()=>{
+//     if(intervalId != 0){
+//       return;
+//     }
+//     intervalId = writeIntervalText(element, prefix, sufix, text);
+//   }
 
-  element.onmouseleave = ()=>{
-    if(intervalId != 0){
-      clearInterval(intervalId);
-      intervalId = 0;
-    }
-    element.innerHTML = initialHTML;
-  }
-}
+//   element.onmouseleave = ()=>{
+//     if(intervalId != 0){
+//       clearInterval(intervalId);
+//       intervalId = 0;
+//     }
+//     element.innerHTML = initialHTML;
+//   }
+// }
 
 function registerMouseClickWriteIntevalText(element, prefix, sufix, text, href){
   let intervalId = 0;
@@ -32,12 +32,15 @@ function writeIntervalText(element, prefix, sufix, text, href){
   const words = text.split('');
   let i = 0;
   let str = "";
-  return setInterval(()=>{
+  let intervalId = setInterval(()=>{
     if(i >= words.length){
+      clearInterval(intervalId);
       window.location.href = href;
       return;
     }
     str += words[i++];
     element.innerHTML = prefix + str + sufix;
   }, 140);
+
+  return intervalId;
 }
