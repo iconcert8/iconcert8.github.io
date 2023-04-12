86d4a38cca77f691329810cb0ce3deec5fac4feb
iconcert8 <51566869+iconcert8@users.noreply.github.com>
2023-04-12 21:07:20 +0900

    FIX: mobile interval error

--- js/interval-text.js
+++ js/interval-text.js
@@ -16,31 +16,36 @@
 //   }
 // }
 
-function registerMouseClickWriteIntevalText(element, prefix, sufix, text, href){
+let registerMouseClickWriteIntevalText = (element, prefix, sufix, text, href)=>{
   let intervalId = 0;
 
   element.onclick = ()=>{
     if(intervalId != 0){
       return;
     }
-    element.style.backgroundColor = '#D8D8D8';
     intervalId = writeIntervalText(element, prefix, sufix, text, href);
   }
-}
 
-function writeIntervalText(element, prefix, sufix, text, href){
-  const words = text.split('');
-  let i = 0;
-  let str = "";
-  let intervalId = setInterval(()=>{
-    if(i >= words.length){
-      clearInterval(intervalId);
-      window.location.href = href;
-      return;
-    }
-    str += words[i++];
-    element.innerHTML = prefix + str + sufix;
-  }, 140);
-
-  return intervalId;
+  let writeIntervalText = (element, prefix, sufix, text, href)=>{
+    const initialHTML = element.innerHTML;
+    const initialStyle = element.style;
+    element.style.backgroundColor = '#D8D8D8';
+    const words = text.split('');
+    let i = 0;
+    let str = "";
+    intervalId = setInterval(()=>{
+      if(i >= words.length){
+        clearInterval(intervalId);
+        i = 0;
+        element.innerHTML = initialHTML;
+        element.style = initialStyle;
+        str = "";
+        intervalId = 0;
+        window.location.href = href;
+        return;
+      }
+      str += words[i++];
+      element.innerHTML = prefix + str + sufix;
+    }, 140);
+  }
 }
